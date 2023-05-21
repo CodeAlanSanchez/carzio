eecdc568f2f00728acede9325c1a70ed23d6230b
Alan Sanchez <realalansan@gmail.com>
2023-05-20 23:04:02 -0700

    Create testimonials styles and optimize other styling

--- resources/js/Components/Home/Testimonials.tsx
+++ resources/js/Components/Home/Testimonials.tsx
@@ -2,7 +2,12 @@ export const Testimonials = () => {
     return (
         <section className="testimonials" id="testimonials">
             <div className="testimonials-content">
-
+                <h1 className="testimonials-header">
+                    Testimonials
+                </h1>
+                <p className="testimonials-text">
+                    Testimonials
+                </p>
             </div>
         </section>
     )
