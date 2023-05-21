4c5cb0ccdd2d4ee7e865320b7453e72e63b0f8c9
Alan Sanchez <realalansan@gmail.com>
2023-05-21 12:27:46 -0700

    Finish basic testimonials

--- resources/js/Components/Home/Testimonials.tsx
+++ resources/js/Components/Home/Testimonials.tsx
@@ -5,9 +5,35 @@ export const Testimonials = () => {
                 <h1 className="testimonials-header">
                     Testimonials
                 </h1>
-                <p className="testimonials-text">
-                    Testimonials
-                </p>
+                <div className="testimonials-row">
+                    <div className="testimonials-container">
+                        <div className="testimonials-name">
+                            Alex
+                        </div>
+                        <div className="testimonials-text">
+                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </div>
+                    </div>
+                    <div className="testimonials-container">
+                        <div className="testimonials-name">
+                            Brian
+                        </div>
+                        <div className="testimonials-text">
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur.
+                        </div>
+                    </div>
+                    <div className="testimonials-container">
+                        <div className="testimonials-name">
+                            Carl
+                        </div>
+                        <div className="testimonials-text">
+                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                            anim id est laborum.
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
