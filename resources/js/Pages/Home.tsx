c98f6a67c6ca6906e21cb33ce2e1d06ff993e420
Alan Sanchez <realalansan@gmail.com>
2023-05-21 16:12:38 -0700

    Add favicon and website title

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -6,10 +6,12 @@ import About from "@/Components/Home/About";
 import Services from "@/Components/Home/Services";
 import Testimonials from '@/Components/Home/Testimonials'
 import Contact from "@/Components/Home/Contact";
+import { Head } from "@inertiajs/react";
 
 export const Home = ({ auth }: PageProps) => {
     return (
         <Main auth={ auth }>
+            <Head title="Home"/>
             <Hero/>
             <Services/>
             <About/>
